ac3767f4625d4277f44c9383e7b31eaf86b6116c
Tom Harrop <twharrop@gmail.com>
2017-02-24 14:11:37 +1300

    sort scaffolds in biopython

---
+++ scripts/remove_short_scaffolds.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from Bio import SeqIO
+
+scaffolds_file = 'dataraw/waspass.fasta'
+
+# make a sorted list of tuples of scaffold name and sequence length
+length_id_unsorted = ((len(rec), rec.id) for
+                      rec in SeqIO.parse(scaffolds_file, 'fasta'))
+length_and_id = sorted(length_id_unsorted)
+
+# get a shortest-to-longest iterator of scaffolds > 1 kb
+scaffolds_over_1kb = reversed([
+    id for (length, id) in length_and_id
+    if length > 999])
+
+# release scaffolds_file from memory
+del(length_and_id)
+
+# build an index of the fasta file
+record_index = SeqIO.index(scaffolds_file, 'fasta')
+
+# write selected records in correct order to disk
+selected_records = (record_index[id] for id in scaffolds_over_1kb)
+SeqIO.write(sequences=selected_records,
+            handle='dataraw/scaffolds_sorted.fasta',
+            format='fasta')
