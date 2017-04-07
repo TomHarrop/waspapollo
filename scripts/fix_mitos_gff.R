b2bc02697f38e0151e184c8b54185db20d76bcc6
Tom Harrop <twharrop@gmail.com>
2017-04-07 15:42:53 +1200

    change strategy, repair then merge

--- scripts/fix_mitos_gff.R
+++ scripts/fix_mitos_gff.R
@@ -10,24 +10,25 @@ mitos_gff1 <- rtracklayer::import.gff1(mitos_gff_file)
 # fix chromosome names
 seqlevels(mitos_gff1) <- "Vvul-mtDNA"
 
-# move type to ID
-mcols(mitos_gff1)$ID <- mcols(mitos_gff1)$type
-
-# fix mcols
+# make a copy of mcols
 mtdna_mcols <- as.data.table(mcols(mitos_gff1))
 
+# move the "type" field to ID and Name
 mtdna_mcols[, ID := as.character(type)]
 mtdna_mcols[, Name := as.character(type)]
 
+# parse the MITOS gene names for sequence type
 mtdna_mcols[grep("^trn", type), new_type := "tRNA"]
 mtdna_mcols[grep("^rrn", type), new_type := "rRNA"]
 mtdna_mcols[is.na(new_type), new_type := "exon"]
 
+# overwrite type with new_type and delete new_type
 mtdna_mcols[, type := new_type][, new_type := NULL]
 
-# insert fixed mcols
+# replace mcols with modified version
 mcols(mitos_gff1) <- data.frame(mtdna_mcols)
 
+# write output to gff3
 rtracklayer::export(object = mitos_gff1,
                     con = "dataraw/vvmtdna/vvul_mitos_fixed.gff",
                     format = "gff3",
