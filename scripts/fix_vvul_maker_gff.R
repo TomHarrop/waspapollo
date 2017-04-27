537d794ac010500f1d7bd2f3c8ba8c3c14984aa9
Tom Harrop <twharrop@gmail.com>
2017-04-27 16:12:35 +1200

    doch

--- scripts/fix_vvul_maker_gff.R
+++ scripts/fix_vvul_maker_gff.R
@@ -1,13 +1,17 @@
 #!/usr/bin/env Rscript
 
+library(GenomicRanges)
+
 # load the gff1
+rutils::GenerateMessage("Loading GFF")
 vvul_gff_file = "dataraw/vvul/waspass.all.vvul.gff"
 vvul_gff3 <- rtracklayer::import.gff3(vvul_gff_file)
 
 # subset feature
-unique(vvul_gff3$type)
+rutils::GenerateMessage("Subsetting")
 keep_features <- c("gene", "mRNA", "exon", "CDS")
 filtered_gff <- vvul_gff3[vvul_gff3$type %in% keep_features]
 
 # export result
+rutils::GenerateMessage("Writing filtered GFF3")
 rtracklayer::export.gff3(filtered_gff, "dataraw/vvul/maker_filtered.gff3")
