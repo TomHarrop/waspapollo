38eef653eb686816241c2d640d5c56788c663cab
Tom Harrop <twharrop@gmail.com>
2017-04-27 16:07:51 +1200

    unneeded libs

--- scripts/fix_vvul_maker_gff.R
+++ scripts/fix_vvul_maker_gff.R
@@ -1,8 +1,5 @@
 #!/usr/bin/env Rscript
 
-library(GenomicRanges)
-library(data.table)
-
 # load the gff1
 vvul_gff_file = "dataraw/vvul/waspass.all.vvul.gff"
 vvul_gff3 <- rtracklayer::import.gff3(vvul_gff_file)
@@ -13,4 +10,4 @@ keep_features <- c("gene", "mRNA", "exon", "CDS")
 filtered_gff <- vvul_gff3[vvul_gff3$type %in% keep_features]
 
 # export result
-rtracklayer::export.gff3(filtered_gff, "dataraw/vvul/filtered.gff3")
+rtracklayer::export.gff3(filtered_gff, "dataraw/vvul/maker_filtered.gff3")
